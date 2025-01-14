b6cc6ed736518438bd5aec6f8943d38826cd665e
SHAHID-BOOP <mohdshahida891@gmail.com>
2025-01-14 10:48:24 +0530

    finish

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int teaLeave=  50;
+  float waterTemp= 85.5;
+  double priceofTea= 299.99;
+  char teaGrade ='A';
+  bool isTeaReady = false;
+
+  cout<< priceofTea <<endl;
+  
+
+  return 0;
+}
---
+++ 02_primitives/modifier.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+  unsigned smallTeaPack = 100;
+
+  float largeTEaStorage = 1000000000;
+
+  cout << largeTEaStorage << endl;
+
+  return 0;
+}
